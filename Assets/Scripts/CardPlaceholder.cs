ca0bfa17e0a802b3b992e689c63bd22d0fded4c5
ocruze <33525693+ocruze@users.noreply.github.com>
2020-12-15 15:24:15 +0100

    fix
    Co-Authored-By: slerandy <75361722+slerandy@users.noreply.github.com>

--- Assets/Scripts/CardPlaceholder.cs
+++ Assets/Scripts/CardPlaceholder.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class CardPlaceholder : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         this.gameObject.SetActive(false);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Scripts/InteractionShowHide.cs
+++ Assets/Scripts/InteractionShowHide.cs
@@ -16,16 +16,4 @@ public class InteractionShowHide : Interaction
     {
         publicObject.SetActive(!publicObject.activeSelf);
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
