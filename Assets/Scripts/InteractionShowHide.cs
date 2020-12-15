a267c999cdd7b4bc72297e935b6366b804b84a10
Samuel <samuel.lerandy@gmail.com>
2020-12-15 15:02:35 +0100

    implement show/hide play/pause video

---
+++ Assets/Scripts/InteractionShowHide.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionShowHide : Interaction
+{
+    public GameObject publicObject = null;
+
+    public override void Interact()
+    {
+        HideOrShowComponent();
+    }
+
+    private void HideOrShowComponent()
+    {
+        publicObject.SetActive(!publicObject.activeSelf);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/InteractionVideo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class InteractionVideo : Interaction
+{
+    public VideoPlayer videoPlayer = null;
+    public override void Interact()
+    {
+        PlayOrPauseVideo();
+    }
+
+    public void Start()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void PlayOrPauseVideo()
+    {
+        if (videoPlayer.isPlaying)
+            videoPlayer.Pause();
+        else
+            videoPlayer.Play();
+    }
+}
